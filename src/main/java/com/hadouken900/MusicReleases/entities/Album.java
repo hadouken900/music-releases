287969716c5826a2e37fbab7f83256490a6789e6
hadouken900 <57232605+hadouken900@users.noreply.github.com>
2021-01-04 02:14:34 +0300

    asdfasdfasdfasdf

--- src/main/java/com/hadouken900/MusicReleases/entities/Album.java
+++ src/main/java/com/hadouken900/MusicReleases/entities/Album.java
@@ -7,7 +7,7 @@ import javax.persistence.*;
 public class Album {
 
     @Id
-    //@GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name="date")
